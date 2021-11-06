ab2acc7d7b1bfe18424ce3b3ce79a9bc0821f2cf
Maxie Bernt <maxie.bernt@hotmail.de>
2021-11-06 17:53:54 +0100

    FIXED button bugs

--- Assets/Scripts/Menu/WeaponInfo/WeaponLevelInfo.cs
+++ Assets/Scripts/Menu/WeaponInfo/WeaponLevelInfo.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponLevelInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator Animator;
 
-    // Update is called once per frame
-    void Update()
+    public void Refresh(bool isOn)
     {
-        
+
     }
 }
