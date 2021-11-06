05c048b254e1a51d52a4c0042555fc244b72c90f
Distel04 <chrisi.teubner@gmail.com>
2021-11-06 01:16:49 +0100

    Added Movement

---
+++ Assets/Scripts/Movement/MovementController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StickyGuns.Movement
+{
+    public class MovementController : MonoBehaviour
+    {
+
+        #region Properties
+
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float playerSpeed;
+
+        private Vector2 movement;
+
+        #endregion
+
+
+        #region Update
+
+        /// <summary>
+        /// Get the Keyboard Inputs
+        /// </summary>
+        private void Update()
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+
+        #endregion
+
+
+        #region FixedUpdate
+
+        /// <summary>
+        /// Move The Player
+        /// </summary>
+        private void FixedUpdate()
+        {
+
+            double verticalHeightSeen = Camera.main.orthographicSize;
+            double horizontalHeightSeen = verticalHeightSeen * Screen.width / Screen.height;
+
+            Vector3 newPositon = rb.position + movement * playerSpeed * Time.deltaTime;
+
+            if (newPositon.x > horizontalHeightSeen - 0.5)
+            {
+                newPositon.x = (float)horizontalHeightSeen - 0.5f;
+            }
+
+            if (newPositon.x < -horizontalHeightSeen + 0.5)
+            {
+                newPositon.x = (float)-horizontalHeightSeen + 0.5f;
+            }
+
+            if (newPositon.y > verticalHeightSeen - 0.5)
+            {
+                newPositon.y = (float)verticalHeightSeen - 0.5f;
+            }
+
+            if (newPositon.y < -verticalHeightSeen + 0.5)
+            {
+                newPositon.y = (float)-verticalHeightSeen + 0.5f;
+            }
+
+            rb.MovePosition(newPositon);
+        }
+
+        #endregion
+
+    }
+}
+
