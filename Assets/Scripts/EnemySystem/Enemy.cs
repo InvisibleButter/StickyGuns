9b23499ec30911c6a53ccef940321337583ba852
Pascal Helmerich <p.helmerich@upjers.com>
2021-11-06 13:27:38 +0100

    Add Enemies

---
+++ Assets/Scripts/EnemySystem/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class Enemy : Entity
+{
+    public float speed = 2f;
+    public float distanceFactor = 0.2f;
+
+    protected new void Start()
+    {
+        base.Start();
+        OnAfterDeath += RemoveEnemy;
+        StartCoroutine(MoveToRandomPos());
+    }
+
+    private IEnumerator MoveToRandomPos()
+    {
+        if (!IsDeath)
+        {
+            Vector3 targetPos = CreateRandomPos();
+            float distance = transform.position.sqrMagnitude - targetPos.sqrMagnitude;
+            float duration = ( Mathf.Abs(distance) + 1 ) * distanceFactor * speed;
+
+            Tween tween = transform.DOMove(targetPos, duration);
+            yield return tween.WaitForCompletion();
+
+            StartCoroutine(MoveToRandomPos());
+        }
+    }
+
+
+    private Vector3 CreateRandomPos()
+    {
+        float x = UnityEngine.Random.Range(-5, 5);
+        float y = UnityEngine.Random.Range(-4, 4);
+
+        return new Vector3(x, y, transform.position.z);
+    }
+
+    private void RemoveEnemy()
+    {
+        StartCoroutine(WaitAndDo(1, () => Destroy(gameObject)));
+    }
+
+    IEnumerator WaitAndDo(float time, Action action)
+    {
+        yield return new WaitForSeconds(time);
+        action();
+    }
+}
---
+++ Assets/Scripts/EnemySystem/EnemySpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public int minRespawnDuration;
+    public int maxRespawnDuration;
+
+    public int targetEnemyAmount;
+    public Transform enemyHolder;
+    public Enemy enemyPrefab;
+    public Transform spawnPoint;
+
+    public int livingEntitys;
+
+    public static EnemySpawner Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            livingEntitys = 0;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SchedulRespawn());
+    }
+
+    IEnumerator SchedulRespawn()
+    {
+        int waitForRespawn = Random.Range(minRespawnDuration, maxRespawnDuration);
+        yield return new WaitForSeconds(waitForRespawn);
+
+        if(livingEntitys < targetEnemyAmount)
+        {
+            Enemy enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, enemyHolder);
+            livingEntitys++;
+            enemy.OnAfterDeath += OnEnemyKilled;
+            StartCoroutine(SchedulRespawn());
+        }
+    }
+
+    void OnEnemyKilled()
+    {
+        livingEntitys--;
+        StartCoroutine(SchedulRespawn());
+    }
+}
