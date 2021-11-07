0a035f108a7a5b0ca957f71aae11426bc6ce0cc5
Leif-Eric <leif-eric.nordmann@web.de>
2021-11-07 01:31:33 +0100

    Mergefix

--- Assets/Scripts/BulletSystem/Bullet.cs
+++ Assets/Scripts/BulletSystem/Bullet.cs
@@ -15,8 +15,8 @@ public class Bullet : MonoBehaviour
     {
         Standard,
         Split,
-<<<<<<< Updated upstream
-        TargetLock
+        TargetLock,
+        Laser
     }
 
     public virtual Type BulletType => Type.Standard;
@@ -64,56 +64,6 @@ public class Bullet : MonoBehaviour
     private void FixedUpdate()
     {
         if(_isActive)
-=======
-        TargetLock,
-        Laser
-    }
-
-    public virtual Type BulletType => Type.Standard;
-
-    public bool IsActive 
-    { 
-        get => _isActive;
-        set 
-        {
-            _isActive = value;
-            if(!_isActive)
-            {
-                DeInitBullet();
-            }
-        } 
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.layer == shootFromLayer || collision.gameObject.layer == gameObject.layer)
-        {
-            return;
-        }
-
-        IsActive = false;
-        Entity entity = collision.gameObject.GetComponent<Entity>();
-        if(entity != null)
-        {
-            entity.TakeDamage(Damage);
-        }
-    }
-
-    public virtual void InitBullet(Transform t, int damage, int shootFromLayer)
-    {
-        _weaponTrans = t;
-        Damage = damage;
-        gameObject.SetActive(true);
-        transform.position = t.position;
-        transform.rotation = t.rotation;
-
-        this.shootFromLayer = shootFromLayer;
-    }
-
-    private void FixedUpdate()
-    {
-        if(_isActive)
->>>>>>> Stashed changes
         {
             //RigidBody.AddForce(transform.up * Speed);
             RigidBody.velocity = transform.up * Speed;
