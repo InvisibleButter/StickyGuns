65770805d802edcb136629a8e3996108b4a48cc7
Maxie Bernt <maxie.bernt@hotmail.de>
2021-11-07 16:56:53 +0100

    ADDED gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public StartMenu Menu;
+    public EnemySpawner Spawner;
+
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Debug.Log("DUPLICATED Game MANAGER SEE YOU SOON BABOON");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        Spawner.SpawnEnemies();
+    }
+
+    public void LooseGame()
+    {
+        Menu.ShowLoose();
     }
 }
+
+
